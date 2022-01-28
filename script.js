d381fe981bfd69aee25db55aa49830ab50869f87
Alien-code-94 <95094133+Alien-code-94@users.noreply.github.com>
2022-01-28 02:30:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
+const togglecolor1 = function() {
+  const bodyElement =document.body;
+  bodyElement.classList.toggle("olive-background");
+  };
+  const changeColorButton1 = document.getElementById("olive");
+  
+   changeColorButton1.addEventListener("click",() => {
+    console.log('olive-background');
+  
+      togglecolor1();
+    });
 
-
-
-const togglecolor2 = function () {
-  const bodyElement = document.body;
+const togglecolor2 = function() {
+  const bodyElement =document.body;
   bodyElement.classList.toggle("red-background");
-};
-const attachEventToChangeColorButton2 = function () {
-  const changeColorButton = document.getElementById("red");
-  changeColorButton.addEventListener("click", function () {
-    togglecolor2();
-  });
-};
+  };
+  const changeColorButton2 = document.getElementById("red");
+  
+   changeColorButton2.addEventListener("click",() => {
+    console.log('red-background');
+  
+      togglecolor2();
+    });
+    
+    const togglecolor3 = function () {
+      const bodyElement = document.body;
+      bodyElement.classList.toggle("blue-background");
+    };
+    const changeColorButton3 = document.getElementById("blue");
+    
+    changeColorButton3.addEventListener("click", () => {
+      console.log('blue-background');
+    
+        togglecolor3();
+      });
+
+      const togglecolor4 = function() {
+        const bodyElement =document.body;
+        bodyElement.classList.toggle("grey-background");
+        };
+        const changeColorButton4 = document.getElementById("grey");
+        
+         changeColorButton4.addEventListener("click",() => {
+          console.log('grey-background');
+        
+            togglecolor4();
+          });
+
+          const togglecolor5 = function() {
+            const bodyElement =document.body;
+            bodyElement.classList.toggle("purple-background");
+            };
+            const changeColorButton5 = document.getElementById("purple");
+            
+             changeColorButton5.addEventListener("click",() => {
+              console.log('purple-background');
+            
+                togglecolor5();
+              });
+ 
 
 
-const togglecolor3 = function () {
-  const bodyElement = document.body;
-  bodyElement.classList.toggle("blue-background");
-};
-const attachEventToChangeColorButton3 = function () {
-  const changeColorButton = document.getElementById("blue");
-  changeColorButton.addEventListener("click", function () {
-    togglecolor3();
-  });
-};
+    
 
-attachEventToChangeColorButton2();
-attachEventToChangeColorButton3();
+  
+
+
+
 
 
 
